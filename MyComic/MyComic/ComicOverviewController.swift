d07f87bc665373299605b09747221d6dd8010765
Alexander Gattringer <a.gattringer@live.at>
2016-01-03 14:31:19 +0100

    basic setup done

---
+++ MyComic/MyComic/ComicOverviewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  MyComic
+//
+//  Created by Alexander Gattringer on 29/12/15.
+//  Copyright © 2015 Alexander Gattringer. All rights reserved.
+//
+
+import UIKit
+
+class ComicOverviewController: UITableViewController{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.title = "Comics"
+    }
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 5
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch (section){
+        case 0:
+            return "xkcd"
+        case 1:
+            return "nerfnow"
+        case 2:
+            return "xmbc"
+        default:
+            return "error"
+        }
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCellWithIdentifier("comicCell")!
+    }
+
+}
+
