f6178f7b793b3bd604fd1b48168a0eb0ae2decc3
Alexander Gattringer <a.gattringer@live.at>
2016-01-19 12:21:39 +0100

    now using scroll view for zooming into comic image

--- MyComic/MyComic/ComicDetailViewController.swift
+++ MyComic/MyComic/ComicDetailViewController.swift
@@ -9,9 +9,10 @@
 import UIKit
 import Haneke
 
-class ComicDetailViewController : UIViewController {
+class ComicDetailViewController : UIViewController, UIScrollViewDelegate {
     var comic: ComicProtocol
     @IBOutlet weak var comicImageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     init(comic:ComicProtocol){
         self.comic = comic
@@ -20,12 +21,29 @@ class ComicDetailViewController : UIViewController {
         self.title = comic.comicName
     }
     
+    override func viewDidLoad() {
+        let comicFormat = Format<UIImage>(name: "comics", diskCapacity: 100 * 1024 * 1024) { image in
+            return image
+        }
+        
+        comicImageView.hnk_setImageFromURL(comic.comicImageSrc, format: comicFormat)
+        
+        self.scrollView.minimumZoomScale = 0.5;
+        self.scrollView.maximumZoomScale = 6.0;
+        self.scrollView.contentSize = comicImageView.bounds.size;
+        
+        self.scrollView.scrollEnabled = true;
+        self.scrollView.delegate = self
+    }
+    
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return self.comicImageView
+    }
+    
     //we are not nscoding compliant
     required init(coder: NSCoder) {
         fatalError("NSCoding not supported")
     }
     
-    override func viewDidLoad() {
-        comicImageView.hnk_setImageFromURL(comic.comicImageSrc)
-    }
+    
 }
